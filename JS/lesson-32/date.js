5b8b4059d81f0880703dae4f6d32377f25b31566
azamjonbro <azamjondasturchi11@gmail.com>
2025-05-17 15:07:53 +0500

    added lesson 33

---
+++ JS/lesson-32/date.js
@@ -0,0 +1,10 @@
+function localeString(date){
+ let day= date.getDate()
+ let month = date.getMonth()+1
+ let year = date.getFullYear()
+  return `${day}-${month}-${year}`
+ 
+ 
+}
+
+export default localeString
