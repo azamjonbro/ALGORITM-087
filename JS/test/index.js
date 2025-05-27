4dbfe26273ed4e80be16905371c6ab89b8db8a6a
azamjonbro <abdullayevazamjon49@gmail.com>
2025-05-27 22:57:39 +0500

    added

--- JS/test/index.js
+++ JS/test/index.js
@@ -10,3 +10,43 @@ overview.addEventListener('click', function(event) {
    overview.style.display = 'none';
  }
 })
+
+// let productList = document.querySelector(".productlist");
+// function showProduct(products) {
+//   productList.innerHTML = "";
+//   products.length == 0
+//     ? (productList.innerHTML = "Product not found")
+//     : products.forEach((item, index) => {
+//         let product = document.createElement("div");
+//         product.classList.add("product");
+
+//         product.innerHTML += `
+//         <img width="400px" src="${item.image}" alt="${item.title}"/>
+//         <h2>${item.title}</h2>
+//       `;
+//         productList.appendChild(product);
+//       });
+// }
+
+// let allproduct = [];
+// function getProducts() {
+//   fetch("https://fakestoreapi.com/products")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       allproduct = data;
+//       showProduct(allproduct);
+//     });
+// }
+// getProducts();
+
+// let input = document.querySelector("#searchInput");
+
+// input.addEventListener("input", (event) => {
+//   console.log(event.target.value);
+//   let qiymat = event.target.value.toLowerCase();
+
+//   let filtered = allproduct.filter((item) =>
+//     item.title.toLowerCase().includes(qiymat)
+//   );
+//   showProduct(filtered);
+// });
