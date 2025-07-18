0a190c26192a377410f13a25f0f00b7166c3206e
azamjonbro <azamjondasturchi11@gmail.com>
2025-07-18 06:13:17 +0500

    added

---
+++ react/libraries/src/translate.js
@@ -0,0 +1,14 @@
+export const translations = {
+  en: {
+    hello: "Hello",
+    welcome: "Welcome to our site!",
+  },
+  uz: {
+    hello: "Salom",
+    welcome: "Saytimizga xush kelibsiz!",
+  },
+  ru: {
+    hello: "Привет",
+    welcome: "Добро пожаловать на наш сайт!",
+  },
+};
