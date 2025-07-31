b818cd4c665407b95d0e565501da6f653917138c
azamjonbro <azamjondasturchi11@gmail.com>
2025-07-31 15:39:34 +0500

    added

---
+++ VUE/crm_file_structure/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template >
+ <div>
+  <h1>Welcome to the Login Page</h1>
+  <p>This is the main content of the login page.</p>
+  <router-link to="/">Click Me!</router-link>
+  <p>You can add more content here as needed.</p>
+  <p>This page can serve as a welcome message to users.</p>
+ </div>
+</template>
+<script>
+export default {
+ 
+}
+</script>
+<style >
+ 
+</style>
