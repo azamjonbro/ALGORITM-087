87c40d8d41d1697b99a9a02aa5094910129feeb0
azamjonbro <azamjondasturchi11@gmail.com>
2025-08-05 02:03:45 +0500

    added

--- VUE/lesson1/src/App.vue
+++ VUE/lesson1/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <Helloikkinchi title="Nima gaplar okalar" :products="abubakrArray" />
+  <Helloikkinchi title="Nima gaplar okalar" :products="filteredProducts" />
+  <input type="text" v-model="inputValue" @input="updateQuery" />
 </template>
 <script>
 import Helloikkinchi from "./components/Helloikkinchi.vue";
@@ -10,7 +11,31 @@ export default {
   data() {
     return {
       abubakrArray: [],
+      inputValue: "",
     };
+  },
+  computed: {
+  filteredProducts() {
+    return this.abubakrArray.filter((item) => {
+      
+      const matchesInput = this.inputValue
+        ? item.title.toLowerCase().includes(this.inputValue.toLowerCase())
+        : true;
+      return matchesPrice && matchesInput;
+    });
+  },
+},
+  watch: {
+    abubakrArray(newValue, oldValue) {
+      console.log("Old value:", oldValue);
+      console.log("New value:", newValue);
+    },
+    '$route.query.inputValue': {
+      handler(newVal) {
+        console.log('Input value changed:', newVal);
+      },
+      immediate: true,
+    },
   },
   methods: {
     async getAllProducts() {
@@ -21,9 +46,20 @@ export default {
           this.abubakrArray = abubakr;
         });
     },
+    updateQuery() {
+      this.$router.replace({
+        query: {
+          ...this.$route.query,
+          inputValue: this.inputValue,
+        },
+      });
+    },
   },
   mounted() {
     this.getAllProducts();
+    if (this.$route && this.$route.query && this.$route.query.inputValue) {
+      this.inputValue = this.$route.query.inputValue;
+    }
   },
 };
 </script>
